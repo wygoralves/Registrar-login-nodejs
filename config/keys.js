23ae7558d0d2173834d4768c9b9f26d86aaa9cc6
Wygor Alves <wygor96@gmail.com>
2019-04-28 22:10:51 -0300

    Banco mongo conectado!

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const dbpassword = "mongodb+srv://wygor96:wyaa9957@portfoliotest-kxtjw.mongodb.net/test?retryWrites=true"
+
+module.exports = { 
+    MongoURI: dbpassword
+};
--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
 const request = require("request");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 const app = express();
 
+//MongoDB
+const db = require("./config/keys").MongoURI;
+mongoose.connect(db, { useNewUrlParser:true })
+    .then(()=> console.log("Banco MongoDB conectado!"))
+    .catch(err => console.log(err));
 //EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
